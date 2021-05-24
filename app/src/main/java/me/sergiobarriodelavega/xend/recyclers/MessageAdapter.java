9f18f05efc0bf1e5f42cbf73d20bf0f78c4b07a0
BuildTools <sergiobarriodelavega@gmail.com>
2021-05-24 18:29:17 +0200

    Now viewholder_message's shows the Date (day, month and year). Updated MessageAdapter.java for said feature.

--- app/src/main/java/me/sergiobarriodelavega/xend/recyclers/MessageAdapter.java
+++ app/src/main/java/me/sergiobarriodelavega/xend/recyclers/MessageAdapter.java
@@ -12,6 +12,8 @@ import org.jxmpp.jid.Jid;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 import me.sergiobarriodelavega.xend.R;
 import me.sergiobarriodelavega.xend.room.ChatLog;
@@ -20,7 +22,7 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHold
     private ArrayList<ChatLog> messages;
     private Jid remoteUserJID;
     private static final int LOCAL_MSG = 0, REMOTE_MSG = 1;
-    private static final SimpleDateFormat formatTime = new SimpleDateFormat("HH:mm");
+    private static final SimpleDateFormat formatTime = new SimpleDateFormat("HH:mm"), formatDate = new SimpleDateFormat("dd/MM/YYYY");
 
     public MessageAdapter(ArrayList<ChatLog> messages, Jid remoteUserJID) {
         this.remoteUserJID = remoteUserJID;
@@ -47,9 +49,34 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHold
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        ChatLog chatLog = messages.get(position);
+        ChatLog chatLog = messages.get(position), previousChatLog;
         holder.tvMessageText.setText(chatLog.msg);
         holder.tvMessageTime.setText(formatTime.format(chatLog.date));
+
+        //Show date
+        if(position != 0){
+            previousChatLog = messages.get(position - 1);
+            if(previousChatLog == null || !compareDates(chatLog.date, previousChatLog.date)){
+                holder.tvDate.setText(formatDate.format(chatLog.date));
+            } else {
+                holder.tvDate.setVisibility(View.GONE);
+            }
+        } else {
+            holder.tvDate.setText(formatDate.format(chatLog.date));
+        }
+
+    }
+
+    private static boolean compareDates(Date date1, Date date2){
+        Calendar cal1, cal2;
+        cal1 = Calendar.getInstance();
+        cal1.setTime(date1);
+        cal2 = Calendar.getInstance();
+        cal2.setTime(date2);
+
+        return cal1.get(Calendar.DAY_OF_MONTH) ==cal2.get(Calendar.DAY_OF_MONTH) &&
+                cal1.get(Calendar.MONTH) ==cal2.get(Calendar.MONTH) &&
+                cal1.get(Calendar.YEAR) ==cal2.get(Calendar.YEAR);
     }
 
     @Override
@@ -58,13 +85,14 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHold
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        private final TextView tvMessageText, tvMessageTime;
+        private final TextView tvMessageText, tvMessageTime, tvDate;
 
         public ViewHolder(View view) {
             super(view);
             // Define click listener for the ViewHolder's View
             tvMessageText = view.findViewById(R.id.tvMessageText);
             tvMessageTime = view.findViewById(R.id.tvMessageTime);
+            tvDate= view.findViewById(R.id.tvDate);
         }
     }
 
